147bc82ef8aed914e7bf3ed9e751ec1a44b12b69
Mehebul02 <mehebul2123@gmail.com>
2025-03-11 23:45:21 +0600

    added the dashboard dynamic name shw

--- src/app/(withDashboard)/dashboard/page.tsx
+++ src/app/(withDashboard)/dashboard/page.tsx
@@ -1,7 +1,22 @@
-const DashboardPage = () => {
+import { authOptions } from "@/utils/authOptions";
+import { getServerSession } from "next-auth";
+import Image from "next/image";
+
+const DashboardPage = async() => {
+  const session = await getServerSession(authOptions)
   return (
     <div>
-      <h1 className="text-4xl text-center mt-10">Welcome To Dashboard Page</h1>
+     {
+      session?.user &&
+      <>
+       <h1 className="text-4xl text-center mt-10">Welcome To {session?.user?.name}</h1>
+      <h1 className="text-4xl text-center mt-10">Email:{session?.user?.email}</h1>
+      <div className="flex justify-center">
+      <Image src={session?.user?.image || "/default-profile.png"} alt="profile" width={200} height={200} 
+      className="border-2 border-green-800 rounded-full text-center my-6"/>
+      </div>
+      </>
+     }
     </div>
   );
 };
